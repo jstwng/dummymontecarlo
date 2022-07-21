339cd0bb814bd584d8adefbd739d979bd4942e19
justin wang <85368198+jstwng@users.noreply.github.com>
2022-07-20 21:06:38 -0400

    Update dummymontecarlo.R

--- dummymontecarlo.R
+++ dummymontecarlo.R
@@ -1,15 +1,15 @@
 library(tidyverse)
 
-RUNS <- 1000
-DECISION.STEPS <- 12
+RUNS = 1000
+DECISION.STEPS = 12
 
-calculate_return <- function(alpha) {
-  risk_free_rate <- 1.03
-  risky_rate <- rnorm(1) * 0.05 + 1
+calculate_return = function(alpha) {
+  risk_free_rate = 1.03
+  risky_rate = rnorm(1) * 0.05 + 1
   (1 - alpha) * risk_free_rate + alpha * risky_rate
 }
 
-simulations <- rerun(RUNS, replicate(DECISION.STEPS, runif(1) %>% calculate_return())) %>%
+simulations = rerun(RUNS, replicate(DECISION.STEPS, runif(1) %>% calculate_return())) %>%
   set_names(paste0("sim", 1:RUNS)) %>%
   map(~ accumulate(., ~ .x * .y)) %>%
   map_dfr(~ tibble(value = .x, step = 1:DECISION.STEPS), .id = "simulation")
